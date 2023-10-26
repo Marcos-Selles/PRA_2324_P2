cb3a1b9c517938a489a2681ac09e77125ef4f98c
MSELSOL <mselsol@alumno.upv.es@ldsic-vdi06.upvnet.upv.es>
2023-10-26 15:25:26 +0200

    Finalizada la parte de Búsqueda Binaria Inversa

--- DyV.h
+++ DyV.h
@@ -20,3 +20,21 @@ int BusBin(valor val, vector<valor>& vect,int ini, int fin){
 		}
 	}
 }
+
+template <typename valor2>
+
+int BusBinInv(valor2  val, vector<valor2>& vect,int ini, int fin){
+        if(ini > fin){
+                return -1;
+        }
+        int posMedia = (ini + fin)/2;
+        if(vect[posMedia] == val){
+                return posMedia;
+        }else{
+                if(vect[posMedia] < val){
+                        return BusBinInv(val,vect,ini,posMedia-1);
+                }else
+                        return BusBinInv(val,vect,posMedia+1,fin);
+                }
+        }
+
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,18 +1,29 @@
 #include "DyV.h"
 #include <iostream>
-
 using namespace std;
-
 int main(){
+        vector<int> s = {11,12,13,14,15,16,17,18,19,20};
+        cout << "Busqueda entero 12 en posicion: "<<BusBin(12,s,0,s.size()) << endl;
 
-        vector<int> a = {1,2,3,4,5,6,7,8,9,10};
-        cout << "Busqueda entero 2 en posicion: "<<BusBin(2,a,0,a.size()) << endl;
+        vector<char> r = {'a','b','c','d','e','f','g','h','i','j'};
+        cout << "Busqueda char g en posicion: "<<BusBin('g',r,0,r.size()) << endl;
 
-	vector<char> b = {'a','b','c','d','e','f','g','h','i','j'};
-        cout << "Busqueda char f en posicion: "<<BusBin('f',b,0,b.size()) << endl;
+        vector<double> t = {1.25,2.86,4.5,6.7,8.1,9.7,10.25,11.4,13.56,17.25};
+        cout << "Busqueda float 6.7 en posicion: "<<BusBin(6.7,t,0,t.size()) << endl;
+        cout << "Busqueda float 8.8 en posicion: "<<BusBin(8.8,t,0,t.size()) << endl;
 
-	vector<double> c = {1.0,1.5,2.0,2.2,3.5,5.0,6.5,7.2,9.11,10.3};
-        cout << "Busqueda float 9.11 en posicion: "<<BusBin(9.11,c,0,c.size()) << endl;
+
+cout << endl << "Busqueda invertida:" << endl;
+
+	vector<int> i = {20,19,18,17,16,15,14,13,12,11};
+        cout << "Busqueda entero 13 en posicion: "<<BusBinInv(13,i,0,i.size()) << endl;
+
+        vector<char> j = {'j','i','h','g','f','e','d','c','b','a'};
+        cout << "Busqueda char g en posicion: "<<BusBinInv('g',j,0,j.size()) << endl;
+
+        vector<double> k = {17.25,13.56,11.4,10.25,9.7,8.1,6.7,4.5,2.86,1.25};
+        cout << "Busqueda float 6.7 en posicion: "<<BusBinInv(6.7,k,0,k.size()) << endl;
 
         return 0;
 }
+
